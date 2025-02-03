68e0a96798e0dd9c1c02fd8aaf6b14b603f040b6
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-02-03 11:19:23 +0530

    Add files via upload

---
+++ S4_C10_E10_Student_class.java
@@ -0,0 +1,39 @@
+/*
+ * Question - 10
+Create a Student class with the following attributes:
+name (String)
+rollNumber (int)
+marks (float)
+
+The class should include:
+
+1. A constructor to initialize all the fields.
+2. A method to display student details.
+3. Overload a method called updateMarks to update marks in two ways:
+- By providing the new marks as a float value.
+- By providing the percentage increase in marks (as a float).
+
+Also, create a main method to:
+1. Input details of a student.
+2. Update marks using both methods.
+3. Display the updated details.
+
+Example Input and Output:
+
+Enter student details:
+Name: John
+Roll Number: 101
+Marks: 85.5
+
+Update marks:
+New Marks: 90.5
+Updated Details: Name: John, Roll Number: 101, Marks: 90.5
+
+Update marks by percentage increase:
+Percentage: 10
+Updated Details: Name: John, Roll Number: 101, Marks: 99.55
+ */
+
+public class S4_C10_E10_Student_class {
+    
+}
