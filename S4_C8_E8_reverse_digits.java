737994d12daa526016d2c3aae4e536acf32c9a4a
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-02-03 11:16:08 +0530

    Add files via upload

---
+++ S4_C8_E8_reverse_digits.java
@@ -0,0 +1,16 @@
+/*
+ * Question - 08
+Write a program that reverses the digits of a given integer. The program should handle negative integers correctly and return the reversed integer. If the reversed integer overflows the 32-bit signed integer range, return 0.
+
+Input: N = 123
+
+Output: 321
+
+Input 2: N = -456
+
+Output 2: -654
+ */
+
+public class S4_C8_E8_reverse_digits {
+    
+}
