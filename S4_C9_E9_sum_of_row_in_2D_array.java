f135921f4449e37a158ce8b829cb1427582b3481
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-02-03 11:17:44 +0530

    Add files via upload

---
+++ S4_C9_E9_sum_of_row_in_2D_array.java
@@ -0,0 +1,23 @@
+/*
+ * Question - 09
+Write a Java program to find the row with the maximum sum in a 2D array.
+
+Input the number of rows and columns in the array.
+Take user input to populate the 2D array.
+Use a for-each loop to iterate over the array and calculate the sum of each row.
+Print the row number (1-based indexing) and the corresponding sum of the row with the maximum sum.
+
+Input:
+3 4
+1 2 3 4
+5 6 7 8
+9 10 11 12
+
+Output:
+Row with the maximum sum: 3
+Maximum sum: 42
+ */
+
+public class S4_C9_E9_sum_of_row_in_2D_array {
+    
+}
