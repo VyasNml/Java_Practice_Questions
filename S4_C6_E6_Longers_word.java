8ad27b89f3cc2998a272b93b36d5a4c1d604e10d
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-02-03 11:10:33 +0530

    Add files via upload

---
+++ S4_C6_E6_Longers_word.java
@@ -0,0 +1,12 @@
+/*
+ * Question - 06
+Write a program that takes a sentence as input and finds the longest word in the sentence. If there are multiple
+ words with the same maximum length, return the first one. Use String class methods and loops to solve the problem.
+
+Input: Coding is a fun challenge.
+Ouput: challenge
+ */
+
+public class S4_C6_E6_Longers_word {
+    
+}
