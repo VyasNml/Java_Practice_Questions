9943a8afbe859d9ff86cad696e611e34fcfeba76
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-02-03 11:14:10 +0530

    Add files via upload

---
+++ S4_C7_E7_string_rotatioin.java
@@ -0,0 +1,15 @@
+/*
+ * Question - 07
+Write a program to check if one string is a rotation of another string. A string
+S2 is a rotation of S1 if it can be obtained by moving some leading characters of
+S1 to its end, while keeping the order of characters intact.
+
+Input: S1 = ""coding""
+S2 = ""ingcod""
+
+Output: true
+ */
+
+public class S4_C7_E7_string_rotatioin {
+    
+}
