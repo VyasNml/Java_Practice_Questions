068256803751623cb29d288c04cb78bb3deadd49
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-02-19 16:31:14 +0530

    Add files via upload

---
+++ S4_C24_E24_majority_number.java
@@ -0,0 +1,11 @@
+/*
+ * Question - 24
+Problem: Given an array, find the majority element (element appearing more than n/2 times). Use the Boyer-Moore Voting Algorithm.
+
+Example:
+Input: [3, 3, 4, 2, 3, 3, 3]
+Output: 3
+ */
+public class S4_C24_E24_majority_number {
+    
+}
---
+++ S4_C25_E25_Sieve_of_Eratosthenes_algorithm.java
@@ -0,0 +1,12 @@
+/*
+ * Question - 25
+Problem: Given an integer N, print all prime numbers up to N using the Sieve of Eratosthenes algorithm.
+
+Example:
+Input: N = 20
+Output: 2, 3, 5, 7, 11, 13, 17, 19
+ */
+
+public class S4_C25_E25_Sieve_of_Eratosthenes_algorithm {
+    
+}
